88d3df317972bac69d46acf5d1119538258b3a7c
edyeftimie <eftimie.eduard28@gmail.com>
2025-01-13 04:37:14 +0200

    untested log repo

---
+++ non_native/lib/src/data/repository/log_repository.dart
@@ -0,0 +1,27 @@
+class LogRepository {
+  final List<Map<String, dynamic>> _logs_queue;
+
+  LogRepository() : _logs_queue = [];
+
+  void addLog(Map<String, dynamic> log) {
+    _logs_queue.add(log);
+  }
+
+  List<Map<String, dynamic>> getLogs() {
+    return _logs_queue;
+  }
+
+  Map<String, dynamic> getFirstLog() {
+    if (!_logs_queue.isEmpty) {
+      var first_log = _logs_queue.first;
+      _logs_queue.removeAt(0);
+      return first_log;
+    }
+
+    String message = "No logs available";
+    var map = Map<String, dynamic>();
+    map["message"] = message;
+
+    return map;
+  }
+}
